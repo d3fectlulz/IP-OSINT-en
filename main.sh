e4fc12fa94bbc0dd983344c22ddb5ed94f5c5a6a
d3fectlulz <jordanyates@beekmancharter.org>
2024-12-06 14:05:54 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ echo -e "\e[1;37m-------------------------------------------\e[0m"
 echo -n -e "\e[1;32mDo you want to return to the menu?\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
 read option
 case $option in
-si)
+yes)
 #! /bin/bash
 bash osint.sh
 exit
