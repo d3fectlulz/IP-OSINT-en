493b429fbb36b94892361e71823bb892a032d083
d3fectlulz <jordanyates@beekmancharter.org>
2024-12-06 14:05:43 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,9 +2,9 @@
 while :
 do
 echo -e "\e[1;37m-------------------------------------------\e[0m" 
-echo -n -e "\e[1;32mDesea regresar al menu\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
-read opcion
-case $opcion in
+echo -n -e "\e[1;32mDo you want to return to the menu?\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
+read option
+case $option in
 si)
 #! /bin/bash
 bash osint.sh
